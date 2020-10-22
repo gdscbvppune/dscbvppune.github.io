6d55a23fb3913cc1b9dc6060a26c37ea8fa103d0
crazy6901 <harshkothari17@gmail.com>
2020-10-23 01:36:48 +0530

    Home-section done

---
+++ src/HomeSection/homeSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './homeSection.css'
+import heroImage from "./heroImage.png"
+import { Container,Row,Col,Button,Image} from 'react-bootstrap';
+
+function Home(){
+    return(
+        <section className="home-section">
+            <Container>
+                <Row>
+                    <Col lg={6} md={12} sm={12} className="text-left">
+                        <h1 className="home-header">Hello, <font className="there-style">There!</font></h1>
+                        <h2 className="home-subheader">We are DSC BVP Pune</h2>
+                        <p className="home-para">Developer Students Club BVP is a community of budding 
+                        developers, programmers and designers, where the 
+                        students follow and learn from fellow developers.</p>
+                        
+                        <Button className="cta-button">Hack BVP</Button>
+                    </Col>
+                    <Col lg={6} md={12} sm={12}>
+                        <Image className="img-fluid" src={heroImage}></Image>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+export default Home;
