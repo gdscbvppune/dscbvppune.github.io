ff86a58e6b5beb7b2ee5f3b9ce84edd035ba44f1
Priyanshu Agarwal <priyanshu.agarwal88@gmail.com>
2020-10-25 04:12:11 +0530

    started team members component

---
+++ src/Components/TeamPage/sections/Members/Members.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Members.css"
+import { Container, Row, Col, Button, Image } from 'react-bootstrap';
+
+
+class Members extends React.Component {
+    render() {
+        return (
+            <div className="members-container">
+            </div>
+        );
+    }
+}
+
+
+export default Members;
