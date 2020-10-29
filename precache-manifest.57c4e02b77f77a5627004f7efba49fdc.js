11b81cbaaf36ae6faad2be688dd4fc5c7bdaa5c6
dewanshrawat15 <dewanshrawat15@gmail.com>
2020-10-29 03:52:20 +0000

    Deploying to gh-pages from  @ 89ec05021f9f54ca1cc759fd00df526a8733c2aa 🚀

---
+++ precache-manifest.57c4e02b77f77a5627004f7efba49fdc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e1f0a3cea836f29500ef5624f52841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e7e84c9ebc3e802be74",
+    "url": "/static/css/main.f79c150d.chunk.css"
+  },
+  {
+    "revision": "161b3ca34878f77f52ae",
+    "url": "/static/js/2.316f9b38.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/static/js/2.316f9b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e7e84c9ebc3e802be74",
+    "url": "/static/js/main.4ad88b56.chunk.js"
+  },
+  {
+    "revision": "dd997ef993200be8a1c3",
+    "url": "/static/js/runtime-main.3a529792.js"
+  },
+  {
+    "revision": "1b0cd58c3f59d6fa3747f71db7d464ef",
+    "url": "/static/media/contact-model.1b0cd58c.webp"
+  },
+  {
+    "revision": "cae1b8c2b9273ee444e4fd42b238d3a4",
+    "url": "/static/media/events-model.cae1b8c2.webp"
+  },
+  {
+    "revision": "c59bdb464d9fbbdd042d4f909e3fbe43",
+    "url": "/static/media/hero-model.c59bdb46.webp"
+  },
+  {
+    "revision": "7685d110db44124f2cde0b487a4101a9",
+    "url": "/static/media/projects-model.7685d110.webp"
+  },
+  {
+    "revision": "1663637f13a9fecebba4e176b38626df",
+    "url": "/static/media/team-model.1663637f.webp"
+  }
+]);
