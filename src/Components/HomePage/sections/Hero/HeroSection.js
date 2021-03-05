57a3ef2320353b458f9498e81e100b6317f39048
Nishchayverma <nishchayverma20@gmail.com>
2021-03-05 14:52:12 +0530

    Link to odetocode removed.

--- src/Components/HomePage/sections/Hero/HeroSection.js
+++ src/Components/HomePage/sections/Hero/HeroSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './HeroSection.css';
 import '../../HomeLeftSection.css';
 import { Container, Row, Col, Button, Image } from 'react-bootstrap';
-import { Link  } from "react-router-dom";
+// import { Link  } from "react-router-dom";
 import heroImage from '../../../../Assets/Images/hero-model.webp';
 
 function Home() {
@@ -19,9 +19,9 @@ function Home() {
                         build technical solutions to the everyday
                         life problems. To inculcate the culture of technology,we organise various
                          workshops and webinars. At DSC we work with a zeal to bring the best from the best. </p>
-                        <Link to="/OdeToCode">
+                        {/* <Link to="/OdeToCode">
                         <Button className="cta-button">Ode To Code</Button>
-                        </Link>
+                        </Link> */}
                     </Col>
                     <Col lg={6} md={12} sm={12}>
                         <Image className="img-fluid hero-image" src={heroImage} ></Image>
