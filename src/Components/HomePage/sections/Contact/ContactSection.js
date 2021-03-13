903ca48d899ec382238f7ef853252fbc41025635
Dewansh Rawat <dewanshrawat15@gmail.com>
2021-03-13 21:31:56 +0530

    Updating website structure

--- src/Components/HomePage/sections/Contact/ContactSection.js
+++ src/Components/HomePage/sections/Contact/ContactSection.js
@@ -17,14 +17,14 @@ function ContactSection() {
                     </Col>
                     <Col lg={6} md={12} sm={12} className="first">
                         <h1 className="desktop-header Contact-header section-heading">Contact <span style={{ color: "#F9BB08" }}>Us</span></h1>
-                        <p className="Contact-para">Feel free to reach out to us using any of <br></br>the social
-                            media networks.
-                            </p>
+                        <p className="Contact-para">Feel free to reach out to us on any of our social media platforms.</p>
 
-                        <div className="social mt-8">
+                        <div className="social">
                             <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/dsc.pune"><i className="fab fa-facebook-f"></i></a>
                             <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/dscbvppune"><i className="fab fa-instagram"></i></a>
                             <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/developer-student-clubs-bvp-pune/"><i className="fab fa-linkedin"></i></a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/dscbvppune"><i className="fab fa-github"></i></a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://dscbvppune.medium.com/"><i className="fab fa-medium"></i></a>
                         </div>
 
                     </Col>
